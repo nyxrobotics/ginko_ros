58988972697f68b544b2d46a47bf4919c9ee163c
Unknown <takaaki.numai01@g.softbank.co.jp>
2019-07-11 17:57:06 +0900

    不要なファイルを削除

--- ginko_description/gazebo/sdf_generate.sh
+++
@@ -1,5 +0,0 @@
-rosrun xacro xacro --inorder -o ./sdf/robot_sim.urdf ./urdf_sim/robot_gazebo.urdf.xacro
-gz sdf -p ./sdf/robot_sim.urdf > ./sdf/robot_sim.sdf
-
-rosrun xacro xacro --inorder -o ./sdf/robot_viz.urdf ./urdf_viz/robot_gazebo.urdf.xacro
-gz sdf -p ./sdf/robot_sim.urdf > ./sdf/robot_viz.sdf
--- ginko_description/gazebo/xacro/sdf_urdf_generator.sh
+++ ginko_description/gazebo/xacro/sdf_urdf_generator.sh
@@ -1,19 +1,27 @@
-ginko_obj_path="$(rospack find ginko_description)/meshes/obj/"
-ginko_stl_path="$(rospack find ginko_description)/meshes/stl/"
-echo $ginko_obj_path
-echo $ginko_stl_path
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:ginko_obj_path:ginko_stl_path
-rosrun xacro xacro --inorder -o  ./urdf_sim/robot_if_rviz.urdf "./urdf_sim/robot_if.xacro" mode:=rviz
-rosrun xacro xacro --inorder -o  ./urdf_sim/robot_if_left.urdf "./urdf_sim/robot_if.xacro" mode:=left_arm
-rosrun xacro xacro --inorder -o  ./urdf_sim/robot_if_right.urdf "./urdf_sim/robot_if.xacro" mode:=right_arm
-gz sdf -p ./urdf_sim/robot_if_rviz.urdf > ./sdf/robot_if_rviz.sdf
-gz sdf -p ./urdf_sim/robot_if_left.urdf > ./sdf/robot_if_left.sdf
-gz sdf -p ./urdf_sim/robot_if_right.urdf > ./sdf/robot_if_right.sdf
-diff -U2  ./sdf/robot_if_rviz.sdf ./sdf/robot_if_left.sdf > ./sdf/robot_if_add_left.patch
-diff -U2  ./sdf/robot_if_rviz.sdf ./sdf/robot_if_right.sdf > ./sdf/robot_if_add_right.patch
-cp ./sdf/robot_if_rviz.sdf ./sdf/robot_if_right_left.sdf
-patch -u ./sdf/robot_if_right_left.sdf < ./sdf/robot_if_add_left.patch
-patch -u ./sdf/robot_if_right_left.sdf < ./sdf/robot_if_add_right.patch
-rosrun pysdf sdf2urdf.py ./sdf/robot_if_right_left.sdf ./sdf/robot_if_right_left.urdf
-sed -i "s@package://PATHTOMESHES@package://ginko_description@g" ./sdf/robot_if_right_left.urdf
-sed -i "s@obj@dae@g" ./sdf/robot_if_right_left.urdf 
+# Generate .urdf from .xacro
+mkdir -p ./tmp
+mkdir -p ./tmp/urdf
+rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_rviz.urdf			"./ginko.xacro" model_mode:=rviz
+rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_common.urdf		"./ginko.xacro" model_mode:=common
+rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_gazebo_part1.urdf	"./ginko.xacro" model_mode:=gazebo_part1
+rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_gazebo_part2.urdf	"./ginko.xacro" model_mode:=gazebo_part2
+
+# Generate .sdf from .urdf
+mkdir -p ./tmp/sdf
+gz sdf -p ./tmp/urdf/ginko_common.urdf       > ./tmp/sdf/ginko_common.sdf
+gz sdf -p ./tmp/urdf/ginko_gazebo_part1.urdf > ./tmp/sdf/ginko_gazebo_part1.sdf
+gz sdf -p ./tmp/urdf/ginko_gazebo_part2.urdf > ./tmp/sdf/ginko_gazebo_part2.sdf
+# Create diff patch
+diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part1.sdf > ./patch/diff_part1.patch
+diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part2.sdf > ./patch/diff_part2.patch
+# Apply patches
+cp ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazbo.sdf
+patch -u ./tmp/sdf/ginko_gazbo.sdf < ./patch/diff_part1.patch
+patch -u ./tmp/sdf/ginko_gazbo.sdf < ./patch/diff_part2.patch
+# Generate URDF
+# rosrun pysdf sdf2urdf.py ./sdf/robot_if_right_left.sdf ./sdf/robot_if_right_left.urdf
+# sed -i "s@package://PATHTOMESHES@package://ginko_description@g" ./sdf/robot_if_right_left.urdf
+# sed -i "s@obj@dae@g" ./sdf/robot_if_right_left.urdf 
+# Copy generated files
+cp ./tmp/urdf/ginko_rviz.urdf ../urdf/ginko_rviz.urdf
+cp ./tmp/sdf/ginko_gazbo.sdf  ../sdf/ginko_gazbo.sdf
