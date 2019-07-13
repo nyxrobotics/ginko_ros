22721833deaed0fe1cf2ac4879d763d6446beff3
nyxrobotics <takaaki.numai01@g.softbank.co.jp>
2019-07-13 22:13:59 +0900

    sedコマンドで閉ループ部分を置換できないか検証

--- ginko_description/gazebo/xacro/sdf_urdf_generator.sh
+++ ginko_description/gazebo/xacro/sdf_urdf_generator.sh
@@ -8,20 +8,26 @@ rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_gazebo_part2.urdf	"./ginko.xac
 
 # Generate .sdf from .urdf
 mkdir -p ./tmp/sdf
+echo "start common sdf"
 gz sdf -p ./tmp/urdf/ginko_common.urdf       > ./tmp/sdf/ginko_common.sdf
+echo "start part1 sdf"
 gz sdf -p ./tmp/urdf/ginko_gazebo_part1.urdf > ./tmp/sdf/ginko_gazebo_part1.sdf
+echo "start part2 sdf"
 gz sdf -p ./tmp/urdf/ginko_gazebo_part2.urdf > ./tmp/sdf/ginko_gazebo_part2.sdf
 # Create diff patch
-diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part1.sdf > ./patch/diff_part1.patch
-diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part2.sdf > ./patch/diff_part2.patch
+mkdir -p ./tmp/patch
+diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part1.sdf > ./tmp/patch/diff_part1.patch
+diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part2.sdf > ./tmp/patch/diff_part2.patch
 # Apply patches
 cp ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazbo.sdf
-patch -u ./tmp/sdf/ginko_gazbo.sdf < ./patch/diff_part1.patch
-patch -u ./tmp/sdf/ginko_gazbo.sdf < ./patch/diff_part2.patch
+patch -u ./tmp/sdf/ginko_gazbo.sdf < ./tmp/patch/diff_part1.patch
+patch -u ./tmp/sdf/ginko_gazbo.sdf < ./tmp/patch/diff_part2.patch
 # Generate URDF
 # rosrun pysdf sdf2urdf.py ./sdf/robot_if_right_left.sdf ./sdf/robot_if_right_left.urdf
 # sed -i "s@package://PATHTOMESHES@package://ginko_description@g" ./sdf/robot_if_right_left.urdf
 # sed -i "s@obj@dae@g" ./sdf/robot_if_right_left.urdf 
 # Copy generated files
+mkdir -p ../urdf
+mkdir -p ../sdf
 cp ./tmp/urdf/ginko_rviz.urdf ../urdf/ginko_rviz.urdf
-cp ./tmp/sdf/ginko_gazbo.sdf  ../sdf/ginko_gazbo.sdf
+cp ./tmp/sdf/ginko_gazbo.sdf ../sdf/ginko_gazebo.sdf
