782f32a96a630b4419c8e8ab44819ffcf235eaed
nyxrobotics <takaaki.numai01@g.softbank.co.jp>
2019-09-26 15:16:22 +0900

    IMUの向きを変更するフィルタを修正

--- imu_angle_correction/include/angle_correction.h
+++ imu_angle_correction/include/angle_correction.h
@@ -8,7 +8,10 @@
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Vector3.h>
 #include <sensor_msgs/Imu.h>
-
+#include <boost/bind.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <geometry_msgs/TransformStamped.h>
 
 class DriftCorrection {
 private:
@@ -18,11 +21,12 @@ private:
 	ros::Publisher imu_base_pub_;
 	ros::Publisher imu_drift_correct_pub_;
 	//ROS Params
-	//座標変換に使用(ジャイロの向き→ロボットの向き)
-	geometry_msgs::Vector3 fixture_euler_;
+	//座標変換に使用(ジャイロの向き調整)
 	geometry_msgs::Vector3 calib_euler_;
 	//ロボットの向きに合わせたジャイロの位置のリンク
-	std::string parent_link;
+	std::string attached_link;
+	std::string target_link;
+	geometry_msgs::TransformStamped transformDiff;
 
 	double gyro_z_drift_ = 0.;		//算出する現在のドリフト値
 	double gyro_z_stopping_ = 1.;	//1~0の値を取る。これが1に近くなるとドリフト補正を始める。
@@ -33,7 +37,8 @@ private:
 	// ROS Topic Subscriber
 	ros::Subscriber joint_target_sub_;	//関節角度の目標値。停止中の判定に使用。
 	ros::Subscriber joint_sens_sub_;	//関節角度の計測値。停止中の判定に使用。
-
+	boost::shared_ptr<tf2_ros::Buffer> tfBuffer_ptr;
+	boost::shared_ptr<tf2_ros::TransformListener> tfListener_ptr;
 
 public:
 	DriftCorrection(ros::NodeHandle main_nh);
