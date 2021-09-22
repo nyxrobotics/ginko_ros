813f0879dfc059f5a0a048d950a10191c96cf544
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2021-09-22 21:14:17 +0900

    refactoring file path

--- ginko_description/xacro/ginko_rs406/sdf_generation/attach_spring.py
+++ ginko_description/xacro/ginko_rs406/sdf_generation/attach_spring.py
@@ -5,8 +5,8 @@ import codecs
 import string
 import re
 
-in_file  = codecs.open('./tmp/sdf/ginko_common_closed.sdf', 'r', 'utf-8')
-out_file = codecs.open('./tmp/sdf/ginko_common_closed_spring.sdf', 'w', 'utf-8')
+in_file  = codecs.open('./export/sdf/ginko_common_closed.sdf', 'r', 'utf-8')
+out_file = codecs.open('./export/sdf/ginko_common_closed_spring.sdf', 'w', 'utf-8')
 
 all_text = in_file.read()
 out_text = re.sub(r"""
--- ginko_description/xacro/ginko_rs406/sdf_generation/sdf_urdf_generator.sh
+++ ginko_description/xacro/ginko_rs406/sdf_generation/sdf_urdf_generator.sh
@@ -1,27 +1,27 @@
 # Generate .urdf from .xacro
-mkdir -p ./tmp
-mkdir -p ./tmp/urdf
-rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_rviz.urdf			"../ginko.xacro" model_mode:=rviz
-rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_common.urdf		"../ginko.xacro" model_mode:=common
-# rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_gazebo_part1.urdf	"./ginko.xacro" model_mode:=gazebo_part1
-# rosrun xacro xacro --inorder -o  ./tmp/urdf/ginko_gazebo_part2.urdf	"./ginko.xacro" model_mode:=gazebo_part2
+mkdir -p ./export
+mkdir -p ./export/urdf
+rosrun xacro xacro --inorder -o  ./export/urdf/ginko_rviz.urdf			"../ginko.xacro" model_mode:=rviz
+rosrun xacro xacro --inorder -o  ./export/urdf/ginko_common.urdf		"../ginko.xacro" model_mode:=common
+# rosrun xacro xacro --inorder -o  ./export/urdf/ginko_gazebo_part1.urdf	"./ginko.xacro" model_mode:=gazebo_part1
+# rosrun xacro xacro --inorder -o  ./export/urdf/ginko_gazebo_part2.urdf	"./ginko.xacro" model_mode:=gazebo_part2
 
 # Generate .sdf from .urdf
-mkdir -p ./tmp/sdf
+mkdir -p ./export/sdf
 echo "start common sdf"
-gz sdf -p ./tmp/urdf/ginko_common.urdf       > ./tmp/sdf/ginko_common.sdf
+gz sdf -p ./export/urdf/ginko_common.urdf       > ./export/sdf/ginko_common.sdf
 # echo "start part1 sdf"
-# gz sdf -p ./tmp/urdf/ginko_gazebo_part1.urdf > ./tmp/sdf/ginko_gazebo_part1.sdf
+# gz sdf -p ./export/urdf/ginko_gazebo_part1.urdf > ./export/sdf/ginko_gazebo_part1.sdf
 # echo "start part2 sdf"
-# gz sdf -p ./tmp/urdf/ginko_gazebo_part2.urdf > ./tmp/sdf/ginko_gazebo_part2.sdf
+# gz sdf -p ./export/urdf/ginko_gazebo_part2.urdf > ./export/sdf/ginko_gazebo_part2.sdf
 # Create diff patch
-# mkdir -p ./tmp/patch
-# diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part1.sdf > ./tmp/patch/diff_part1.patch
-# diff -U2  ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazebo_part2.sdf > ./tmp/patch/diff_part2.patch
+# mkdir -p ./export/patch
+# diff -U2  ./export/sdf/ginko_common.sdf ./export/sdf/ginko_gazebo_part1.sdf > ./export/patch/diff_part1.patch
+# diff -U2  ./export/sdf/ginko_common.sdf ./export/sdf/ginko_gazebo_part2.sdf > ./export/patch/diff_part2.patch
 # Apply patches
-# cp ./tmp/sdf/ginko_common.sdf ./tmp/sdf/ginko_gazbo.sdf
-# patch -u ./tmp/sdf/ginko_gazbo.sdf < ./tmp/patch/diff_part1.patch
-# patch -u ./tmp/sdf/ginko_gazbo.sdf < ./tmp/patch/diff_part2.patch
+# cp ./export/sdf/ginko_common.sdf ./export/sdf/ginko_gazbo.sdf
+# patch -u ./export/sdf/ginko_gazbo.sdf < ./export/patch/diff_part1.patch
+# patch -u ./export/sdf/ginko_gazbo.sdf < ./export/patch/diff_part2.patch
 # Generate URDF
 python sdf_close.py
 python attach_spring.py
@@ -29,5 +29,5 @@ python attach_spring.py
 # Copy generated files
 mkdir -p ../urdf
 mkdir -p ../sdf
-cp ./tmp/urdf/ginko_rviz.urdf ../urdf/ginko_rviz.urdf
-cp ./tmp/sdf/ginko_common_closed_spring.sdf ../sdf/ginko_gazebo.sdf
+cp ./export/urdf/ginko_rviz.urdf ../urdf/ginko_rviz.urdf
+cp ./export/sdf/ginko_common_closed_spring.sdf ../sdf/ginko_gazebo.sdf
